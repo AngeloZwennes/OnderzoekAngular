b47eab3b7b9a51629db44792ea65a7cc3c4ef01e
Angelo <angelo_93@live.nl>
2017-12-10 16:38:09 +0100

    added routing & basic login

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,25 @@
-import {Component} from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {isNull, isNullOrUndefined} from 'util';
+import {Router} from '@angular/router';
 
 @Component({
     selector: 'app-root',
     templateUrl: './app.component.html',
     styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+    constructor(private router: Router) {
+    }
     title = 'app';
+    username: any;
+
+    ngOnInit() {
+        if (isNullOrUndefined(localStorage.getItem('username'))) {
+            // there is no username available
+            this.router.navigateByUrl('/login');
+        } else {
+            this.router.navigateByUrl('/dashboard');
+            // there is a username available
+        }
+    }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,22 @@ import {NgModule} from '@angular/core';
 
 
 import {AppComponent} from './app.component';
+import {LoginComponent} from './login/login.component';
+import {AppRoutingModule} from './/app-routing.module';
+import {DashboardComponent} from './dashboard/dashboard.component';
+import {FormsModule} from '@angular/forms';
 
 
 @NgModule({
     declarations: [
-        AppComponent
+        AppComponent,
+        LoginComponent,
+        DashboardComponent
     ],
     imports: [
-        BrowserModule
+        BrowserModule,
+        AppRoutingModule,
+        FormsModule
     ],
     providers: [],
     bootstrap: [AppComponent]
