ceaa3a5e3d5ab99d05a7f6ac0166e522a607dcf8
Bodhie <stefanbode1995@hotmail.com>
2017-12-13 11:26:16 +0100

    Family
    Family members
    Photo dialog update

--- src/app/dashboard/photo/photo.component.ts
+++ src/app/dashboard/photo/photo.component.ts
@@ -37,4 +37,23 @@ export class PhotoComponent implements OnInit {
 })
 export class PhotoDialog {
     constructor(@Inject(MAT_DIALOG_DATA) public data: any) {}
+
+    fileChanged(e: Event) {
+        var target: HTMLInputElement = e.target as HTMLInputElement;
+        for(var i=0;i < target.files.length; i++) {
+            this.upload(target.files[i]);
+        }
+    }
+
+    upload(img: File) {
+        var formData: FormData = new FormData();
+        formData.append("image", img, img.name);
+
+        var xhr = new XMLHttpRequest();
+        xhr.upload.addEventListener("progress", (ev: ProgressEvent) => {
+            //You can handle progress events here if you want to track upload progress (I used an observable<number> to fire back updates to whomever called this upload)
+        });
+        xhr.open("PUT", "assets/images/", true);
+        xhr.send(formData);
+    }
 }
