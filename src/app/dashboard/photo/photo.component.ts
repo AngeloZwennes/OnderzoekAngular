8bb30db050a0dcc09749777d4228c278a2fc0e97
Bodhie <stefanbode1995@hotmail.com>
2017-12-19 12:47:36 +0100

    Fix

--- src/app/dashboard/photo/photo.component.ts
+++ src/app/dashboard/photo/photo.component.ts
@@ -1,5 +1,4 @@
 import { Component, OnInit } from '@angular/core';
-import {MatDialog} from '@angular/material';
 import { Photo } from '../../photo';
 import {HttpClient} from '@angular/common/http';
 
