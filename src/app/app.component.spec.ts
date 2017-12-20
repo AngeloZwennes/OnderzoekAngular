7b0fde098c270582258df08ff8356a720746a4bb
Bodhie <stefanbode1995@hotmail.com>
2017-12-20 15:16:03 +0100

    2Unit tests

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,12 +1,51 @@
 import {TestBed, async} from '@angular/core/testing';
+
+import {BrowserModule} from '@angular/platform-browser';
 import {AppComponent} from './app.component';
+import {LoginComponent} from './login/login.component';
+import {AppRoutingModule} from './/app-routing.module';
+import {DashboardComponent} from './dashboard/dashboard.component';
+import {FormsModule, ReactiveFormsModule} from '@angular/forms';
+import {SettingsComponent} from './settings/settings.component';
+import {NavbarComponent} from './navbar/navbar.component';
+import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
+import {UserService} from './user.service';
+import {HttpClientModule} from '@angular/common/http';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import { CalendarComponent } from './calendar/calendar.component';
+import {PhotoComponent} from './dashboard/photo/photo.component';
+import { TaskComponent } from './dashboard/task/task.component';
+import { FamilyComponent } from './dashboard/family/family.component';
+import { GroceriesComponent } from './groceries/groceries.component';
+import {GroceryService} from './grocery.service';
+import { CalendarService } from './calendar.service';
+import {APP_BASE_HREF} from "@angular/common";
 
 describe('AppComponent', () => {
     beforeEach(async(() => {
         TestBed.configureTestingModule({
             declarations: [
-                AppComponent
+                AppComponent,
+                LoginComponent,
+                DashboardComponent,
+                SettingsComponent,
+                NavbarComponent,
+                CalendarComponent,
+                PhotoComponent,
+                TaskComponent,
+                FamilyComponent,
+                GroceriesComponent
             ],
+            imports: [
+                BrowserModule,
+                AppRoutingModule,
+                ReactiveFormsModule,
+                FormsModule,
+                BrowserAnimationsModule,
+                NgbModule.forRoot(),
+                HttpClientModule
+            ],
+            providers: [{provide: APP_BASE_HREF, useValue : '/' },UserService,GroceryService, CalendarService]
         }).compileComponents();
     }));
     it('should create the app', async(() => {
@@ -18,11 +57,5 @@ describe('AppComponent', () => {
         const fixture = TestBed.createComponent(AppComponent);
         const app = fixture.debugElement.componentInstance;
         expect(app.title).toEqual('app');
-    }));
-    it('should render title in a h1 tag', async(() => {
-        const fixture = TestBed.createComponent(AppComponent);
-        fixture.detectChanges();
-        const compiled = fixture.debugElement.nativeElement;
-        expect(compiled.querySelector('h1').textContent).toContain('Welcome to app!');
     }));
 });
